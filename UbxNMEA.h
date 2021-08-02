099ed5c375269269bdcb1bf88615d1a903371213
mr-ingenious <82120163+mr-ingenious@users.noreply.github.com>
2021-08-02 23:32:27 +0200

    Update UbxNMEA.h

--- UbxNMEA.h
+++ UbxNMEA.h
@@ -3,25 +3,29 @@
 
 #include "Arduino.h"
 
+/* ****************************************************************************
+  NMEA message definitions. (WORK IN PROGRESS)
+  ****************************************************************************/
+
 /* ************************************************************************* */
 struct coord {
-	byte deg = 0;
-	float fract = 0.0;
+  byte deg = 0;
+  float fract = 0.0;
 };
 
 /* ************************************************************************* */
 struct UbxNMEA_GPRMC {
-	double utime = 0.0;
-	byte status = 0;
-	coord lat;
-	byte ns_ind = 0;
-	coord lon;
-	byte ew_ind = 0;
-	float spd = 0.0;
-	float cog = 0.0;
-	byte date[3] = { 0 };
-	byte mode = 0;
-	byte checksum[2] = { 0 };
+  double utime = 0.0;
+  byte status = 0;
+  coord lat;
+  byte ns_ind = 0;
+  coord lon;
+  byte ew_ind = 0;
+  float spd = 0.0;
+  float cog = 0.0;
+  byte date[3] = { 0 };
+  byte mode = 0;
+  byte checksum[2] = { 0 };
 };
 
 /* ************************************************************************* */
@@ -46,12 +50,12 @@ struct UbxNMEA_GPVTG {
 
 /* ************************************************************************* */
 struct UbxNMEA_GPDTM {
-	float lat = 0.0;
-	byte ns_ind = 0;
-	float lon = 0.0;
-	byte ew_ind = 0;
-	float alt = 0.0;
-	byte checksum[2] = { 0 };
+  float lat = 0.0;
+  byte ns_ind = 0;
+  float lon = 0.0;
+  byte ew_ind = 0;
+  float alt = 0.0;
+  byte checksum[2] = { 0 };
 };
 
 /* ************************************************************************* */
